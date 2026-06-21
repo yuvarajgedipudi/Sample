8e1bacf24eb29a1426fef12ce429e59f4cfc47e0
yuvarajgedipudi <yuvarajgedipudi@gmail.com>
2026-06-21 19:14:04 +0530

    added new file called Eagle

---
+++ src/Eagle.java
@@ -0,0 +1,31 @@
+public class Eagle {
+
+  public void eagleMethodOne()
+  {
+             int [][] a = new int[2][2];
+
+             a [0][0] = 5;
+             a [0][1] = 10;
+             a [1][0] = 15;
+             a [1][1] = 20;
+
+
+             for(int i=0;i<a.length;i++)
+             {
+                 for(int j=0;j<a.length;j++)
+                 {
+                     System.out.println(a[i][j]);
+                 }
+
+             }
+  }
+    public static void main(String[] args)
+    {
+          Eagle ee = new Eagle();
+          ee.eagleMethodOne();
+
+    }
+
+
+
+}
